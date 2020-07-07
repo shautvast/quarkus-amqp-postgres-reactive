90299707ff708a227c2fb33e19cba34eaef5d00f
Sander Hautvast <shautvast@gmail.com>
2020-07-07 10:49:57 +0200

    cleanup

--- src/main/java/nl/sander/BewegingRepo.java
+++ src/main/java/nl/sander/BewegingRepo.java
@@ -6,10 +6,7 @@ import io.vertx.mutiny.pgclient.PgPool;
 import io.vertx.mutiny.sqlclient.Row;
 import io.vertx.mutiny.sqlclient.RowSet;
 import io.vertx.mutiny.sqlclient.Tuple;
-import io.vertx.pgclient.PgConnectOptions;
-import io.vertx.sqlclient.PoolOptions;
 
-import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
@@ -20,21 +17,6 @@ import java.util.stream.StreamSupport;
 public class BewegingRepo {
     @Inject
     PgPool client;
-
-//    @PostConstruct
-//    public void init() {
-//        PgConnectOptions connectOptions = new PgConnectOptions()
-//                .setPort(5432)
-//                .setHost("localhost")
-//                .setDatabase("postgres")
-//                .setUser("postgres")
-//                .setPassword("sander");
-//
-//        PoolOptions poolOptions = new PoolOptions()
-//                .setMaxSize(2);
-//
-//        client = PgPool.pool(connectOptions, poolOptions);
-//    }
 
     public Uni<Beweging> getBeweging(String id) {
         String sql = "select * from beweging where id = $1";
@@ -55,15 +37,13 @@ public class BewegingRepo {
 
     }
 
-    private Function<Row, Beweging> mapRow() {
-        return row -> new Beweging(row.getLong("id"), row.getLocalDate("vldatum"), row.getString("bewegingcode"));
-    }
-
-
-
     @Transactional
     public void persist(Beweging beweging) {
         client.preparedQuery("insert into beweging(vldatum, bewegingcode) values ($1,$2)")
                 .execute(Tuple.of(beweging.getVlDatum(), beweging.getBewegingcode())).await().indefinitely();
+    }
+
+    private Function<Row, Beweging> mapRow() {
+        return row -> new Beweging(row.getLong("id"), row.getLocalDate("vldatum"), row.getString("bewegingcode"));
     }
 }
